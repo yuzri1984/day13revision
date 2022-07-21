e9f225767d19313148840d8763921fdb1abafc42
yuzri1984 <yuzrifitriah@hotmail.com>
2022-07-21 14:28:30 +0800

    completed revision

--- src/main/java/sg/edu/nus/iss/d13revision/controllers/PersonController.java
+++ src/main/java/sg/edu/nus/iss/d13revision/controllers/PersonController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import sg.edu.nus.iss.d13revision.models.Person;
+import sg.edu.nus.iss.d13revision.models.PersonForm;
 import sg.edu.nus.iss.d13revision.services.PersonService;
 
 @Controller
@@ -49,4 +51,32 @@ public class PersonController {
 
         return "personList";
     }
-}
+
+    @RequestMapping(value="/addPerson", method = RequestMethod.GET)
+    public String showAddPersonPage(Model model){
+        PersonForm pForm = new PersonForm();
+        model.addAttribute("personForm", pForm);
+
+        return "addPerson";
+    }
+        @RequestMapping(value="/addPerson", method = RequestMethod.POST)
+        public String savePerson(Model model,
+            @ModelAttribute("personForm") PersonForm personForm){
+
+                String fName = personForm.getFirstName();
+                String lName = personForm.getLastName();
+
+                if(fName !=null && fName.length() > 0 && lName !=null && lName.length() > 0){
+                    Person newPerson = new Person(fName, lName);
+                    perSvc.addPerson(newPerson);
+
+                    return "redirect:/personList";
+
+                }
+                model.addAttribute("errorMessage", errorMessage);
+                return "addPerson";
+            }
+
+
+
+        }
