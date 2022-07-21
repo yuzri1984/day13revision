8fbe65541b5b12827f24976eee994b4b081e77d3
yuzri1984 <yuzrifitriah@hotmail.com>
2022-07-21 10:01:39 +0800

    changes to master branch

--- src/main/java/sg/edu/nus/iss/d13revision/D13revisionApplication.java
+++ src/main/java/sg/edu/nus/iss/d13revision/D13revisionApplication.java
@@ -1,11 +1,11 @@
 package sg.edu.nus.iss.d13revision;
 
-import java.util.Collections;
-
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.*;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
@@ -13,27 +13,29 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 public class D13revisionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D13revisionApplication.class, args);
+		// SpringApplication.run(D13revisionApplication.class, args);
 
-		SpringApplication app = new SpringApplication( D13revisionApplication.class);
+		SpringApplication app = new SpringApplication(D13revisionApplication.class);
 
-		String  port = "8085";
+		String port="8085";
 		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
 
-		if (cliOpts.containsOption("port")){
+		if (cliOpts.containsOption("port")) {
 			port = cliOpts.getOptionValues("port").get(0);
 		}
-		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+		app.setDefaultProperties(
+			Collections.singletonMap("server.port", port)
+		);
+
 		app.run(args);
 	}
+
 	@Bean
 	public CommonsRequestLoggingFilter log() {
 		CommonsRequestLoggingFilter logger = new CommonsRequestLoggingFilter();
-		logger.setIncludeClientInfo(includeClientInfo: true);
-		logger.setIncludeClientInfo(includeQueryString: true);
+		logger.setIncludeClientInfo(true);
+		logger.setIncludeQueryString(true);
 		return logger;
-
-
-
 	}
 }
